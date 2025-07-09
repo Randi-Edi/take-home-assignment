9e43b8a48227f5536f603a7c7ca9d19e55776cbf
Rushan Edirisuriya <rushan.edirisuriya@gmail.com>
2025-07-09 13:44:23 +0530

    LL-03 add sql queries

---
+++ src/repositories/postRepository.js
@@ -0,0 +1,45 @@
+const db = require("../database/db.js");
+
+class PostRepository {
+  getPostById(id) {
+    return new Promise((resolve, reject) => {
+      db.get(`SELECT * FROM posts WHERE id = ?`, [id], (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+
+  getAllPosts() {
+    return new Promise((resolve, reject) => {
+      db.all(`SELECT * FROM posts`, [], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  savePost(post) {
+    return new Promise((resolve, reject) => {
+      db.run(
+        `INSERT INTO posts (id, userId, title, body) VALUES (?, ?, ?, ?)`,
+        [post.id, post.userId, post.title, post.body],
+        function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(post);
+          }
+        }
+      );
+    });
+  }
+}
+
+module.exports = new PostRepository();
